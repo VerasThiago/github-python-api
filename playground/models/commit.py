ecd6951e90c0b6a351982958dcb5289189b87ff9
Thiago <thiago.verasmachado@coinbase.com>
2022-07-11 22:30:29 -0300

    Update models to handle cache and JSON response

--- playground/models/commit.py
+++ playground/models/commit.py
@@ -2,10 +2,10 @@ import json
 from django.db import models
 from .repo import Repo
 
+
 class Commit(models.Model):
     repo = models.ForeignKey(Repo, on_delete=models.CASCADE)
 
-    cached = models.BooleanField(default=True)
     sha = models.CharField(max_length=128)
     author = models.CharField(max_length=100)
     email = models.EmailField()
@@ -16,17 +16,3 @@ class Commit(models.Model):
 
     def __str__(self) -> str:
         return self.sha
-
-    def toJSON(self):
-        return json.dumps(
-            {
-                "sha": self.sha,
-                "author": self.author,
-                "email": self.email,
-                "date": self.date.__str__(),
-                "message": self.message,
-                "url": self.url,
-                "comment_count": self.comment_count,
-                "cached": self.cached,
-            }
-        )
